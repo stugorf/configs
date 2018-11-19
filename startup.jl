8e0b0a5260055b327e906233dc3e7c0d7d9ba9e8
David Hughes <dhughes@seattlecca.org>
2018-11-19 13:53:32 -0800

    adding Julia startup file

---
+++ startup.jl
@@ -0,0 +1,7 @@
+push!(LOAD_PATH, "$(homedir())/dev")
+push!(LOAD_PATH, "$(homedir())/myr")
+push!(LOAD_PATH, "$(homedir())/fhcrc")
+push!(LOAD_PATH, "$(homedir())/a4c")
+push!(LOAD_PATH, "./")
+push!(LOAD_PATH, "./src")
+
